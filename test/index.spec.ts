5bc08dcaa1c2f3f63a587df943fbaba98f17ebb5
Charles Bergeron <chuck.bergeron@gmail.com>
2021-04-06 15:32:13 -0700

    * make typescript happy

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -3,13 +3,13 @@ import * as evmChainsExtended from '../src'
 describe('evm-chains-extended', () => {
   it('aliases getChain and appends blockExplorerUrl data', () => {
     const chain = evmChainsExtended.getChain(1)
-    expect(chain.name).toEqual('Ethereum Mainnet')
-    expect(chain.blockExplorerUrls).toEqual(['https://etherscan.io'])
+    expect(chain?.name).toEqual('Ethereum Mainnet')
+    expect(chain?.blockExplorerUrls).toEqual(['https://etherscan.io'])
   })
 
   it('aliases getAllChains and appends extra datas', () => {
     const chains = evmChainsExtended.getAllChains()
-    expect(chains[0].name).toEqual('Ethereum Mainnet')
-    expect(chains[0].blockExplorerUrls).toEqual(['https://etherscan.io'])
+    expect(chains?.[0].name).toEqual('Ethereum Mainnet')
+    expect(chains?.[0].blockExplorerUrls).toEqual(['https://etherscan.io'])
   })
 })
